ad35ab37dad8d992c4244f540dd23a968e33d176
unknown <nazwafitrumaulana2gmail.com>
2023-09-16 16:38:46 +0700

    Week 15

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from os.path import join,dirname
 from dotenv import load_dotenv
 
 from flask import Flask, render_template, request, jsonify
-import certifi
+
 from pymongo import MongoClient
 
 dotenv_path = join(dirname(__file__), ".env")
